6c1eded67af871b2e96d053775fea57b0d277417
efinal <yyb@xiaorui.app>
2021-08-30 19:58:52 +0800

    modify for aliyun sms

--- sms77.rb
+++ sms77.rb
@@ -1,40 +1,49 @@
-class Channel::Driver::Sms::Sms77
-  NAME = 'sms/sms77'.freeze
+require 'aliyun/sms'
+class Channel::Driver::Sms::AliyunSMS
+  NAME = 'sms/aliyunsms'.freeze
 
   def send(options, attr, _notification = false)
     Rails.logger.info "Sending SMS to recipient #{attr[:recipient]}"
 
     return true if Setting.get('import_mode')
-
-    Rails.logger.info "Backend sending sms77 SMS to #{attr[:recipient]}"
+    Rails.logger.info "Set Aliyun SMS environment"
+    Aliyun::Sms.configure do |config|
+      config.access_key_secret = options[:access_key_secret]   
+      config.access_key_id = options[:access_key_id]            
+      config.action = 'SendSms'                       # default value
+      config.format = 'XML'                           # http return format, value is 'JSON' or 'XML'
+      config.region_id = 'cn-hangzhou'                # default value      
+      config.sign_name = options[:sign_name]                    
+      config.signature_method = 'HMAC-SHA1'           # default value
+      config.signature_version = '1.0'                # default value
+      config.version = '2017-05-25'                   # default value
+    end
+    Rails.logger.info "Backend sending aliyun SMS to #{attr[:recipient]}"
     begin
-      url = 'https://gateway.sms77.io/api/sms?' + URI.encode_www_form({
-                                                                          p: options[:api_key],
-                                                                          text: attr[:message],
-                                                                          to: attr[:recipient],
-                                                                          from: options[:from],
-                                                                          sendWith: 'zammad',
-                                                                      })
-
+      template_param = {"message"=>attr[:message]}.to_json
+      out_id = ''
       if Setting.get('developer_mode') != true
-        response = Faraday.get(url).body
-        raise response if '100' != response
+        Aliyun::Sms.send(attr[:recipient], options[:template_code], template_param, out_id)
+        Rails.logger.info "Send SMS Response #{out_id}"
+        raise out_id if '100' != out_id
       end
 
       true
     rescue => e
-      Rails.logger.debug "sms77 error: #{e.inspect}"
+      Rails.logger.debug "aliyun SMS send error: #{e.inspect}"
       raise e
     end
   end
 
   def self.definition
     {
-        name: 'sms77',
-        adapter: 'sms/sms77',
+        name: 'aliyunsms',
+        adapter: 'sms/aliyunsms',
         notification: [
-            {name: 'options::api_key', display: 'API Key', tag: 'input', type: 'text', limit: 64, null: false, placeholder: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'},
-            {name: 'options::from', display: 'From', tag: 'input', type: 'text', limit: 16, null: true, placeholder: '00491710000000'},
+            {name: 'options::access_key_secret', display: 'ACCESS KEY SECRET', tag: 'input', type: 'text', limit: 64, null: false, placeholder: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'},
+            {name: 'options::access_key_id', display: 'ACCESS KEY ID', tag: 'input', type: 'text', limit: 64, null: false, placeholder: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'},
+            {name: 'options::sign_name', display: '短信签名', tag: 'input', type: 'text', limit: 16, null: true, placeholder: '【蜂雀科技】'},
+            {name: 'options::template_code', display: '短信模板', tag: 'input', type: 'text', limit: 16, null: true, placeholder: 'xxxxxxxx'},
         ]
     }
   end
